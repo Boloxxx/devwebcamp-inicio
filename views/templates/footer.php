04a5992e59bf352467dab490f775ee0aa04f4c09
Boloxxx <bryanrodriguezalmonte@gmail.com>
2024-02-08 15:58:40 -0400

    Devwebcamp

---
+++ views/templates/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+    <div class="footer__grid">
+        <div class="footer__contenido">
+            <h3 class="footer__logo">
+                &#60;DevWebCamp />
+            </h3>
+
+            <p class="footer__texto">
+                DevWebCamp es una conferencia para desarrolladores de todos los niveles, se lleva a cabo de forma
+                presencial y en linea.
+            </p>
+        </div>
+
+        <nav class="menu-redes">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank"
+                href="https://twitter.com/Bolox_">
+                <span class="menu-redes__ocultar">Twitter</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank"
+                href="https://www.youtube.com/@bolox_">
+                <span class="menu-redes__ocultar">YouTube</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank"
+                href="https://instagram.com/bryanrodr1guez">
+                <span class="menu-redes__ocultar">Instagram</span>
+            </a>
+
+        </nav>
+    </div>
+
+    <p class="footer__copyright">
+        DevWebCamp
+        <span class="footer__copyright--regular">
+            - Todos los derechos resevados
+            <?php echo date('Y') ?>
+        </span>
+    </p>
+</footer>
