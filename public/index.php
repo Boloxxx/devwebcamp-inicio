476f20de19bd2c8d3a5a9399ce85d6352d7df899
Boloxxx <bryanrodriguezalmonte@gmail.com>
2024-02-12 15:57:43 -0400

    v1

--- public/index.php
+++ public/index.php
@@ -1,9 +1,15 @@
-<?php 
+<?php
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\EventosController;
+use Controllers\PonentesController;
+use Controllers\RegalosController;
+use Controllers\RegistradosController;
 use MVC\Router;
 use Controllers\AuthController;
+use Controllers\DashboardController;
+
 
 $router = new Router();
 
@@ -28,6 +34,16 @@ $router->post('/reestablecer', [AuthController::class, 'reestablecer']);
 // Confirmación de Cuenta
 $router->get('/mensaje', [AuthController::class, 'mensaje']);
 $router->get('/confirmar-cuenta', [AuthController::class, 'confirmar']);
+
+// Area de administracion
+$router->get('/admin/dashboard', [DashboardController::class, 'index']);
+
+$router->get('/admin/ponentes', [PonentesController::class, 'index']);
+$router->get('/admin/ponentes/crear', [PonentesController::class, 'crear']);
+
+$router->get('/admin/eventos', [EventosController::class, 'index']);
+$router->get('/admin/registrados', [RegistradosController::class, 'index']);
+$router->get('/admin/regalos', [RegalosController::class, 'index']);
 
 
 $router->comprobarRutas();
